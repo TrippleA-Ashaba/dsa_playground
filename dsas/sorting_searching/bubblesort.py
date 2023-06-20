ad86a0d45d9dd33cb3e995c15889a9ed366457a7
TrippleA-Ashaba <trippleaunit@hotmail.com.com>
2023-06-20 15:24:01 +0300

    bubblesort

---
+++ dsas/sorting_searching/bubblesort.py
@@ -0,0 +1,13 @@
+def bubble_sort(items):
+    for i in range(len(items)):
+        for j in range(len(items) - i - 1):
+            if items[j] > items[j + 1]:
+                items[j], items[j + 1] = items[j + 1], items[j]
+
+    return items
+
+
+if __name__ == "__main__":
+    a = [3, 4, 6, 2, 9, 8, 10, 7, 1, 5]
+    # a = ["a", "c", "f", "d", "i", "b", "g", "h", "e"]
+    print(bubble_sort(a))
